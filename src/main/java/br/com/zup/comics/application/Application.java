ff44e12f8678cb281333f34d5679c13f64acf433
manuela.rodriguez <manuela.rodriguez@dataprev.gov.br>
2021-06-29 14:14:29 -0300

    # Consumindo a marvel api utilizando spring-cloud-feign

--- src/main/java/br/com/zup/comics/application/Application.java
+++ src/main/java/br/com/zup/comics/application/Application.java
@@ -3,6 +3,7 @@ package br.com.zup.comics.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
+@EnableFeignClients(basePackages = {"br.com.zup.comics.feign"})
 @EnableEncryptableProperties
 @SpringBootApplication
 @EnableJpaRepositories("br.com.zup.comics.repository")
@@ -25,5 +27,6 @@ public class Application {
     public RestTemplate buildRestTemplate() {
         return new RestTemplate();
     }
+    
 }
 
